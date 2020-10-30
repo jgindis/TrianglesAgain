4536c1cf3b10cce285c8bef5a0e7d9b6c19bf182
jgindis <jgindis20@stuy.edu>
2020-10-30 10:27:09 -0400

    Added accessors

--- Triangle.java
+++ Triangle.java
@@ -13,7 +13,26 @@ public class Triangle {
 
   public Triangle(double x1, double y1,double x2,
                   double y2,double x3, double y3) {
-
+    v1 = Point.Point(x1, y1);
+    v2 = Point.Point(x2, y2);
+    v3 = Point.Point(x3, y3);
   }
+
+  public double getPerimeter() {
+    //Return the perimeter without any rounding.
+  }
+
+  public double getArea() {
+    //Return the area using Heron's formula without any rounding.
+  }
+
+  public String classify() {
+    //Return the "equilateral" "isosceles" or "scalene" Round distances to the nearest tenthousandths for your classifications.
+  }
+
+  public String toString() {
+    //The format should be "v1(23.0, 4.0) v2(-2.3, 5.001) v3(5.0, 0.52)"
+  }
+
 
 }
