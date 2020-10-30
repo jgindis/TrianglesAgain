bd0baf30bab02fe4fef88850c18c746a599427cb
jgindis <jgindis20@stuy.edu>
2020-10-30 09:44:52 -0400

    Added equals()

--- Point.java
+++ Point.java
@@ -2,23 +2,28 @@ public class Point {
 
   private double x,y;
 
-    public Point(double X, double Y) {
-      x=X;
-      y=Y;
-    }
+  public Point(double X, double Y) {
+      x = X;
+      y = Y;
+  }
 
-    public Point(Point p) {
-    x= p.x;
-    y= p.y;
+  public Point(Point p) {
+      x = p.x;
+      y = p.y;
   }
 
   public double getX() {
      return x;
    }
 
-   public double getY() {
-     return y;
-   }
+  public double getY() {
+    return y;
+  }
 
+  public double distanceTo(Point other) {
+    double distance = Math.sqrt(Math.pow(x - other.x, 2)
+                          + Math.pow(y - other.y, 2));
+    return distance;
+  }
 
 }
