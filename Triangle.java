b39af5f59ab121ddc0c28aeb72d94c4d14e0744b
jgindis <jgindis20@stuy.edu>
2020-10-30 14:56:52 -0400

    Revised functions for efficiency and readability

--- Triangle.java
+++ Triangle.java
@@ -3,43 +3,54 @@ public class Triangle {
   private Point v1, v2, v3;
 
   public Triangle(Point a, Point b, Point c) {
-    x1 = Point.getX(a);
-    x2 = Point.getX(b);
-    x3 = Point.getX(c);
-    y1 = Point.getY(a);
-    y2 = Point.getY(b);
-    y3 = Point.getY(c);
+    v1 = a;
+    v2 = b;
+    v3 = c;
   }
 
   public Triangle(double x1, double y1,double x2,
                   double y2,double x3, double y3) {
-    v1 = Point.Point(x1, y1);
-    v2 = Point.Point(x2, y2);
-    v3 = Point.Point(x3, y3);
+    v1 = new Point(x1, y1);
+    v2 = new Point(x2, y2);
+    v3 = new Point(x3, y3);
   }
 
   public double getPerimeter() {
-    double perimeter = Point.distanceTo(v1)
-                     + Point.distanceTo(v2)
-                     + Point.distanceTo(v3);
-    return perimeter;
+    double s1 = v1.distanceTo(v2);
+    double s2 = v2.distanceTo(v3);
+    double s3 = v3.distanceTo(v1);
+    return (s1 + s2 + s3);
   }
 
   public double getArea() {
-    double p = getPerimeter();
-    double A = Math.sqrt(p*(p-Point.distanceTo(v1))
-                          *(p-Point.distanceTo(v2))
-                          *(p-Point.distanceTo(v3)));
-    return A;
+    double a = v1.distanceTo(v2);
+    double b = v2.distanceTo(v3);
+    double c = v3.distanceTo(v1);
+    double s = getPerimeter()/2;
+    double A = s*(s - a)*(s - b)*(s - c);
+    return(Math.sqrt(A));
   }
 
   public String classify() {
-    //Return the "equilateral" "isosceles" or "scalene" Round distances to the nearest tenthousandths for your classifications.
-  }
+    double s1 = Math.round(((v1.distanceTo(v2)) * 10000.0)/ 10000.0);
+    double s2 = Math.round(((v2.distanceTo(v3)) * 10000.0)/ 10000.0);
+    double s3 = Math.round(((v3.distanceTo(v1)) * 10000.0)/ 10000.0);
+    if (s1 == s2 && s2 == s3) return "equilateral ";
+    if (s1 == s2 || s2 == s2) return "isosceles ";
+    else return "scalene ";
+}
 
   public String toString() {
-    //The format should be "v1(23.0, 4.0) v2(-2.3, 5.001) v3(5.0, 0.52)"
+    String str = "v1"+"("+v1.getX()+","+v1.getY()+") "
+               + "v2"+"("+v2.getX()+","+v2.getY()+") "
+               +"v3"+"("+v3.getX()+","+v3.getY()+")";
+    return str;
   }
 
+  public void setVertex(int index, Point newP) {
+      if (index == 0) v1 = newP;
+      if (index == 1) v2 = newP;
+      if (index == 2) v3 = newP;
+    }
 
 }
